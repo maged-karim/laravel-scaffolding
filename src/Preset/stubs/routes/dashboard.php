53db1fd22951407c09439ce092b18daaded7b528
Maged Karim <elmotahda.software96@gmail.com>
2020-02-12 03:44:09 +0200

    fix issues

--- src/Preset/stubs/routes/dashboard.php
+++ src/Preset/stubs/routes/dashboard.php
@@ -10,5 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'HomeController')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
